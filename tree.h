94453145d38917587ec35047bf42417d0d2cc368
Trisha Khanna <71102170+trisha0708@users.noreply.github.com>
2022-01-11 16:48:18 -0500

    headings changed

--- tree.h
+++ tree.h
@@ -1,20 +1,3 @@
-/*
- Student Name: Trisha Khanna
- Student ID: 1123248
- Due Date: October 26th, 2021
- Course: CIS*2520
- I have exclusive control over this submission via my password.
- By including this header comment, I certify that:
- 1) I have read and understood the policy on academic integrity.
- 2) I have completed Moodle's module on academic integrity.
- 3) I have achieved at least 80% on the academic integrity quiz
- I assert that this work is my own. I have appropriate acknowledged
- any and all material that I have used, be it directly quoted or
- paraphrased. Furthermore, I certify that this assignment was written
- by me in its entirety.
-*/
-
-
 #ifndef _TREE_H
 #define _TREE_H
 #include <stdio.h>
@@ -57,8 +40,8 @@ struct Tree *newTree( struct memsys *memsys, unsigned int width );
 void freeTree( struct memsys *memsys, struct Tree *tree );
 void addItem( struct memsys *memsys, struct Tree *tree, int (*compar)(const void *, const void *), void *src );
 
-/*******************************************
-ADDITIONAL FUNCTIONALITY -- SEARCH FUNCTION:
-*******************************************/
+/*****************
+SEARCH FUNCTION:
+*****************/
 
 int searchItem( struct memsys *memsys, struct Tree *tree, int (*compar)(const void *, const void *), void *target );
